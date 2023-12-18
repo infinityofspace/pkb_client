ae0aff3bfd7a5bb4670cd7097a53d3fafc35941b
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2023-12-18 22:20:00 +0100

    updated client tests

--- tests/client.py
+++ tests/client.py
@@ -5,7 +5,7 @@ from importlib import resources
 from threading import Thread
 
 from pkb_client.client import PKBClient, PKBClientException
-from pkb_client.dns import DNSRecord
+from pkb_client.dns import DNSRecord, DNSRecordType
 from pkb_client.forwarding import URLForwarding, URLForwardingType
 from tests import responses
 from tests.handler import RequestHandler
@@ -13,6 +13,8 @@ from tests.handler import RequestHandler
 BASE_URL = "http://localhost:8000/api/json/v3/"
 
 YOUR_IP = "172.0.0.1"
+KEY = "key"
+SECRET = "secret"
 
 
 class TestClientAuth(unittest.TestCase):
@@ -27,43 +29,43 @@ class TestClientAuth(unittest.TestCase):
         server_thread.start()
 
     def test_valid_auth(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         ip_address = pkb_client.ping()
 
         self.assertEqual(YOUR_IP, ip_address)
 
     def test_invalid_auth(self):
-        pkb_client = PKBClient("keys", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY + "s", SECRET, BASE_URL)
 
         with self.assertRaises(PKBClientException):
             pkb_client.ping()
 
     def test_ping(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         ip_address = pkb_client.ping()
 
         self.assertEqual(YOUR_IP, ip_address)
 
     def test_dns_create(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
-        dns_id = pkb_client.dns_create("example.com", "A", "172.0.0.1", "sub.example.com", 3600, 2)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
+        dns_id = pkb_client.dns_create("example.com", DNSRecordType.A, "172.0.0.1", "sub.example.com", 3600, 2)
 
         self.assertEqual("123456", dns_id)
 
     def test_dns_edit(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
-        success = pkb_client.dns_edit("example.com", "123456", "A", "172.0.0.1", "sub.example.com", 3600, 2)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
+        success = pkb_client.dns_edit("example.com", "123456", DNSRecordType.A, "172.0.0.1", "sub.example.com", 3600, 2)
 
         self.assertTrue(success)
 
     def test_dns_delete(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         success = pkb_client.dns_delete("example.com", "123456")
 
         self.assertTrue(success)
 
     def test_dns_retrieve(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         success = pkb_client.dns_retrieve("example.com")
 
         with resources.open_text(responses, "dns_retrieve.json") as f:
@@ -73,13 +75,13 @@ class TestClientAuth(unittest.TestCase):
         self.assertEqual(records, success)
 
     def test_set_dns_servers(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         success = pkb_client.update_dns_servers("example.com", ["ns1.example.com", "ns2.example.com"])
 
         self.assertTrue(success)
 
     def test_get_url_forward(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         forwardings = pkb_client.get_url_forward("example.com")
 
         with resources.open_text(responses, "domain_getUrlForwarding.json") as f:
@@ -89,7 +91,7 @@ class TestClientAuth(unittest.TestCase):
         self.assertEqual(forwardings, expected_forwards)
 
     def test_add_url_forward(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         success = pkb_client.add_url_forward("example.com",
                                              "sub.example.com",
                                              "https://www.example.com",
@@ -100,7 +102,7 @@ class TestClientAuth(unittest.TestCase):
         self.assertTrue(success)
 
     def test_delete_url_forwarding(self):
-        pkb_client = PKBClient("key", "secret", BASE_URL)
+        pkb_client = PKBClient(KEY, SECRET, BASE_URL)
         success = pkb_client.delete_url_forward("example.com",
                                                 "123456")
 
